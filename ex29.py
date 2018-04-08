7eb54de019bfa1cbf07b0e027a63e27b3819bda8
zhenghaoyang <532425107@qq.com>
2018-04-08 17:41:23 +0800

    ex29
    补上ex29

---
+++ ex29.py
@@ -0,0 +1,27 @@
+# --coding: utf-8 --
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+    print "Too many cats! The world is doomed!"
+if people > cats:
+    print "Not many cats! The world is saved!"
+if people < dogs:
+    print "The world is drooled on!"
+if people > dogs:
+    print    "The world is dry!"
+dogs += 5
+if people >= dogs:
+    print "People are greater than or equals to dogs"
+if people <= dogs:
+    print "People are less than o equal to dogs."
+if people == dogs:
+print "People are dogs."
+
+#加分习题
+#判断
+#rule
+#IndentationError: expected an indented block
+#不同的输出
+
